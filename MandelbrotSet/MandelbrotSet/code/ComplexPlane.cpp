8075668d0c1737a4688f4288fd9ad011a7eb4eba
matty2wavy <matthn1604@gmail.com>
2023-04-27 20:54:26 -0700

    MandelbrotSet

--- MandelbrotSet/MandelbrotSet/code/ComplexPlane.cpp
+++ MandelbrotSet/MandelbrotSet/code/ComplexPlane.cpp
@@ -1,7 +1,11 @@
 #include "ComplexPlane.h"
+#include <SFML/Graphics.hpp>
 #include <complex>
+#include <iostream>
+#include <sstream>
 
 using namespace std;
+using namespace sf;
 
 ComplexPlane::ComplexPlane(float aspectRatio)
 {
@@ -44,10 +48,13 @@ void ComplexPlane::setMouseLocation(Vector2f coord)
 
 void ComplexPlane::loadText(Text& text)
 {
-	//finish later
     ostringstream oss;
-	oss << "Mandelbrot Set" << endl << "Center: (0,0)" << endl << "Cursor: " << endl;
-	text.setString(oss.str());
+    oss << "Mandelbrot Set" << endl;
+    oss << "Center: (" << m_view.getCenter().x << ", " << m_view.getCenter().y << ")" << endl;
+    oss << "Cursor: (" << m_mouseLocation.x << ", " << m_mouseLocation.y << ")" << endl;
+    oss << "Left click to Zoom in" << endl;
+    oss << "Right click to Zoom out" << endl;
+    text.setString(oss.str());
 }
 
 size_t ComplexPlane::countIterations(Vector2f coord)
@@ -65,5 +72,43 @@ size_t ComplexPlane::countIterations(Vector2f coord)
 
 void ComplexPlane::iterationsToRGB(size_t count, Uint8& r, Uint8& g, Uint8& b)
 {
-	//I don't know
+    if (count == MAX_ITER) {
+        // Black color for max iterations
+        r = 0;
+        g = 0;
+        b = 0;
+    }
+    else {
+        // Divide color scheme into 5 regions
+        const size_t region_size = MAX_ITER / 5;
+        const size_t region = count / region_size;
+        const size_t remainder = count % region_size;
+        const Uint8 base_color = (Uint8)((255.0 / region_size) * remainder);
+        if (region == 0) { // Purple/blue
+            r = 200;
+            g = 0;
+            b = base_color + 128;
+        }
+        else if (region == 1) { // Turquoise
+            r = 0;
+            g = base_color + 128;
+            b = 128;
+        }
+        else if (region == 2) { // Green
+            r = 0;
+            g = 128;
+            b = base_color + 128;
+        }
+        else if (region == 3) { // Yellow
+            r = base_color + 128;
+            g = 128;
+            b = 0;
+        }
+        else { // Red
+            r = 200;
+            g = base_color + 128;
+            b = 0;
+        }
+    }
 }
+
--- MandelbrotSet/MandelbrotSet/code/main.cpp
+++ MandelbrotSet/MandelbrotSet/code/main.cpp
@@ -8,100 +8,103 @@ using namespace sf;
 
 int main()
 {
-	unsigned int width = VideoMode::getDesktopMode().width;
-	unsigned int height = VideoMode::getDesktopMode().height;
-	float aspectRatio = height / width;
+    unsigned int width = VideoMode::getDesktopMode().width;
+    unsigned int height = VideoMode::getDesktopMode().height;
+    float aspectRatio = (float)height / (float)width;
 
-	RenderWindow window(VideoMode(width, height), "Mandelbrot Set", Style::Fullscreen);
+    RenderWindow window(VideoMode(width, height), "Mandelbrot Set", Style::Fullscreen);
 
-	ComplexPlane cp(aspectRatio);
-	Font font;
-	Text hud;
-	font.loadFromFile("fonts/UniversCondensed.ttf");
-	hud.setFont(font);
-	hud.setCharacterSize(75);
-	hud.setPosition(20, 20);
-	hud.setFillColor(Color::White);
+    ComplexPlane cp(aspectRatio);
+    Font font;
+    Text hud;
+    font.loadFromFile("fonts/UniversCondensed.ttf");
+    hud.setFont(font);
+    hud.setCharacterSize(20);
+    hud.setPosition(20, 20);
+    hud.setFillColor(Color::White);
 
-	VertexArray vArray(Points, width * height);
+    VertexArray vArray(Points, width * height);
 
-	enum class State { CALCULATING, DISPLAYING };
-	State state = State::CALCULATING;
+    enum class State { CALCULATING, DISPLAYING };
+    State state = State::CALCULATING;
 
-	while (window.isOpen())
-	{
-		/*
-		*********************
-		Handle Input Segment
-		*********************
-		*/
+    while (window.isOpen())
+    {
+        /*
+        *********************
+        Handle Input Segment
+        *********************
+        */
 
-		Event event;
-		while (window.pollEvent(event))
-		{
-			if (event.type == Event::Closed)
-			{
-				window.close();
-			}
-			if (Keyboard::isKeyPressed(Keyboard::Escape))
-			{
-				window.close();
-			}
-			if (event.type == Event::MouseButtonPressed)
-			{
-				Vector2i pixelPos = Mouse::getPosition(window);
-				Vector2f worldPos = window.mapPixelToCoords(pixelPos, cp.getView());
-				if (event.mouseButton.button == Mouse::Left)
-				{
-					cp.zoomIn();
-					cp.setCenter(worldPos);
-					state = State::CALCULATING;
-				}
-				else if (event.mouseButton.button == Mouse::Right)
-				{
-					cp.zoomOut();
-					cp.setCenter(worldPos);
-					state = State::CALCULATING;
-				}
-			}
-			if (event.type == Event::MouseMoved)
-			{
-				Vector2i pixelPos = Mouse::getPosition(window);
-				Vector2f worldPos = window.mapPixelToCoords(pixelPos, cp.getView());
-				cp.setMouseLocation(worldPos);
-			}
-		}
-		/*
-		****************
-		Update the Scene
-		****************
-		*/
+        Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == Event::Closed)
+            {
+                window.close();
+            }
+            if (Keyboard::isKeyPressed(Keyboard::Escape))
+            {
+                window.close();
+            }
+            if (event.type == Event::MouseButtonPressed)
+            {
+                Vector2i pixelPos = Mouse::getPosition(window);
+                Vector2f worldPos = window.mapPixelToCoords(pixelPos, cp.getView());
+                if (event.mouseButton.button == Mouse::Left)
+                {
+                    cp.zoomIn();
+                    cp.setCenter(worldPos);
+                    state = State::CALCULATING;
+                }
+                else if (event.mouseButton.button == Mouse::Right)
+                {
+                    cp.zoomOut();
+                    cp.setCenter(worldPos);
+                    state = State::CALCULATING;
+                }
+            }
+            else if (event.type == Event::MouseMoved)
+            {
+                Vector2i pixelPos = Mouse::getPosition(window);
+                Vector2f worldPos = window.mapPixelToCoords(pixelPos, cp.getView());
+                cp.setMouseLocation(worldPos);
+            }
+        }
 
-		if (state == State::CALCULATING)
-		{
-			int pixelWidth = width;
-			int pixelHeight = height;
-			for (int i = 0; i < pixelHeight; i++)
-			{
-				for (int j = 0; j < pixelWidth; j++)
-				{
-					vArray[j + i * pixelWidth].position = { (float)j,(float)i };
-					Vector2f coord = window.mapPixelToCoords({ j, i }, cp.getView());
-					size_t count = ComplexPlane::countIterations(coord);
-					Uint8 r;
-					Uint8 g;
-					Uint8 b;
-					ComplexPlane::iterationsToRGB(count, r, g, b);
-					vArray[j + i * pixelWidth].color = { r,g,b };
-				}
-			}
-			state = State::DISPLAYING;
-			cp.loadText(hud);
-		}
-		window.clear();
-		window.draw(vArray);
-		window.draw(hud);
-		window.display();
-	}
-	return 0;
+        /*
+        ****************
+        Update the Scene
+        ****************
+        */
+
+        if (state == State::CALCULATING)
+        {
+            int pixelWidth = width;
+            int pixelHeight = height;
+            for (int i = 0; i < pixelHeight; i++)
+            {
+                for (int j = 0; j < pixelWidth; j++)
+                {
+                    vArray[j + i * pixelWidth].position = { (float)j,(float)i };
+                    Vector2f coord = window.mapPixelToCoords({ j, i }, cp.getView());
+                    size_t count = ComplexPlane::countIterations(coord);
+                    Uint8 r = 0;
+                    Uint8 g = 0;
+                    Uint8 b = 0;
+                    cp.iterationsToRGB(count, r, g, b);
+                    vArray[j + i * pixelWidth].position = { (float)j, (float)i };
+                    vArray[j + i * pixelWidth].color = Color(r, g, b);
+                }
+            }
+            state = State::DISPLAYING;
+            cp.loadText(hud);
+        }
+        window.clear();
+        window.draw(vArray);
+        window.draw(hud);
+        window.display();
+    }
+
+    return 0;
 }
